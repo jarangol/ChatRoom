201ddae818fe5e9a6433b25f5978be086c2f3ac3
Julian <jarangol@eafit.edu.co>
2019-01-25 22:33:28 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Socket } from 'ng-socket-io';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+
+export class HomePage {
+  nickname = '';
+
+  constructor(public navCtrl: NavController, private socket: Socket) { }
+ 
+  joinChat() {
+    this.socket.connect();
+    this.socket.emit('set-nickname', this.nickname);
+    this.navCtrl.push('ChatRoomPage', { nickname: this.nickname });
+  }
 }
+
